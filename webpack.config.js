08719ebf3d8a2db21c8b3e2c9ada26a80a17a327
aknur1999 <kae.aknur@gmail.com>
2022-08-10 16:19:28 +0900

    ReactJS - Environment Setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './main.js',
+   output: {
+      path: path.join(__dirname, '/bundle'),
+      filename: 'index_bundle.js'
+   },
+   devServer: {
+      //inline: true,
+      port: 8001
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+            //use: "babel-loader",
+            /*use: {
+                loader: 'babel-loader'
+                /*options: {
+                    presets: ['@babel/preset-env']
+                }
+            }*/
+            /*query: {
+               presets: ['es2015', 'react']
+            }*/
+         }
+      ]
+   },
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: './index.html'
+      })
+   ]
+}
