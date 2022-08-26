c2f9dcf528f666a8cb37c8511126cc107dcdda98
Hoyeon Lee <vnzk0301@naver.com>
2022-08-26 09:32:10 +0200

    first commit

--- src/components/features/Login/index.js
+++ src/components/features/Login/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from "react"
-import Menu from "./Menu"
-import Nav from 'react-bootstrap/Nav';
-import { Link } from "react-router-dom"
-import { BrandButtonDefault } from "../../library/Button"
-import {useLocation} from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import Menu from "./Menu";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import { BrandButtonDefault } from "../../library/Button";
+import { useLocation } from "react-router-dom";
 
 // const SignUpButton = () => {
 //     const [visible, setVisible] = useState(false)
@@ -28,57 +28,56 @@ import {useLocation} from 'react-router-dom';
 // }
 
 const SignUpButton = () => {
-    const location = useLocation();
-     
-    const [visible, setVisible] = useState(false)
+  const location = useLocation();
 
-    const displayMenu = () => {
-        if (title == "Yujin님, 환영합니다") {
-            console.log("sfh");
-            setVisible(true)
-        }
+  const [visible, setVisible] = useState(false);
+
+  const displayMenu = () => {
+    if (title == "Yujin님, 환영합니다") {
+      console.log("sfhyess");
+      setVisible(true);
     }
-    const hideMenu = () => {
-        setVisible(false)
-    }
-    const [title, setTitle] = useState("Login")
+  };
+  const hideMenu = () => {
+    setVisible(false);
+  };
+  const [title, setTitle] = useState("Login");
 
-    useEffect(() => {
-        setTitle(JSON.parse(JSON.stringify(window.localStorage.getItem('title'))));
-      }, []);
-    
-      useEffect(() => {
-        window.localStorage.setItem('title', title);
-      }, [title]);
+  useEffect(() => {
+    setTitle(JSON.parse(JSON.stringify(window.localStorage.getItem("title"))));
+  }, []);
 
+  useEffect(() => {
+    window.localStorage.setItem("title", title);
+  }, [title]);
 
-    const onClick = () => {
-        setTitle("Yujin님, 환영합니다");
-        setShowSignUpButton(false);
-    }
-    const [showSignUpButton, setShowSignUpButton] = useState(true);
+  const onClick = () => {
+    setTitle("Yujin님, 환영합니다");
+    setShowSignUpButton(false);
+  };
+  const [showSignUpButton, setShowSignUpButton] = useState(true);
 
-    return (
+  return (
     <React.StrictMode>
-    <div className="flex xs:hidden items-center ml-auto">
-    {showSignUpButton && <Link to="/signup" className="mr-5">
-        <BrandButtonDefault
-            type="button"
-            value="Sign up"
-        />
-    </Link>}
-    <div className="block"
-        onMouseLeave={hideMenu}>
-        <Nav.Link
-            className="flex text-white xl:mr-6 sm:mr-8 md:mr-8 mr-4" 
-            onMouseOver={displayMenu} 
-            onClick={onClick}>
-                {title}
-        </Nav.Link>
-        <Menu isVisible={visible}/>
-    </div>
-    </div>
+      <div className="flex xs:hidden items-center ml-auto">
+        {showSignUpButton && (
+          <Link to="/signup" className="mr-5">
+            <BrandButtonDefault type="button" value="Sign up" />
+          </Link>
+        )}
+        <div className="block" onMouseLeave={hideMenu}>
+          <Nav.Link
+            className="flex text-white xl:mr-6 sm:mr-8 md:mr-8 mr-4"
+            onMouseOver={displayMenu}
+            onClick={onClick}
+          >
+            {title}
+          </Nav.Link>
+          <Menu isVisible={visible} />
+        </div>
+      </div>
     </React.StrictMode>
-)}
+  );
+};
 
-export default SignUpButton
+export default SignUpButton;
